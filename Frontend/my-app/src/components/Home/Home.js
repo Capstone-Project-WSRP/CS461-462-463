01ac0ff5f1f0a1766f42869bb76c889251e3ad9f
Stacey C <cattells@oregonstate.edu>
2024-03-11 05:47:30 -0700

    updated home page info

--- Frontend/my-app/src/components/Home/Home.js
+++ Frontend/my-app/src/components/Home/Home.js
@@ -10,11 +10,45 @@ const Home = () => {
         This is a Website Security Research Project (WSRP) simulation built with
         React for educational purposes.
       </p>
-      <h1>Instructions</h1>
+      <h2>Introduction</h2>
       <p className={styles.description}>
-        There is no instructions yet because there is no working code as of now
-        but as the project continues there will be more added this part of the
-        application.
+          Web applications are insecure, and due to these vulnerabilities, there have been
+numerous severely damaging data breaches in recent history. Currently, development is
+happening at a rapid pace that security professionals cannot keep up with. There are many
+points of weakness in new tech due to its rapid development and how the threat landscape
+is constantly evolving. Developers and security professionals have a need to fortify against
+these security vulnerabilities for the sake of both their companies and their users. In this
+project, we will study some of the most common vulnerabilities relating to data integrity
+and account breaches in order to create a “how-to” web application and guide, to demonstrate
+these vulnerabilities and how to harden web apps to protect against them.
+        <br></br>
+        <br></br>
+          By presenting clear, step-by-step instructions and immediate access to necessary tools within our platform, we streamline the learning process and eliminate the need for users to navigate external sources. This approach could provide a consolidated and comprehensive resource that significantly reduces search time and ensures users have consistent and reliable guidance at their fingertips. Envision it as a comprehensive hub for beginner-level cybersecurity training that is up in a manner that promotes user-friendliness. Our product's direct guide promotes a more structured and efficient learning experience, offering users a unified platform for learning and applying cybersecurity measures. It also emphasizes self-sufficiency by providing the user with the tools and knowledge to perform the vulnerability. The streamlined design alleviates some challenges inherent in mastering a new, complex skill like cybersecurity.
+        <br></br>
+        <br></br>
+            Because we are designing our product for those at the start of their cybersecurity journey (at least initially), it might be too fundamental for seasoned experts. However, it is important to note that while we focus on teaching a specific method to exploit a vulnerability, users may discover alternative techniques. In that case, we want to encourage others to share such findings, aiding in the growth of community knowledge. The knowledge we learn from this phase of the implementation will aid in the plans to further expand the scope of this product in the future. Perfecting our delivery of this knowledge now will produce a better product in the years to come.
+            <br></br>
+            <br></br>
+            <br></br>
+
+      </p>
+      <h2>Instructions</h2>
+      <p className={styles.description}>
+        Please begin by navigating to the security vulnerability you would like to learn more about using the menu to the left.
+        Once on an individual vulnerability's page, you may read about the vulnerability, why it is important
+        to protect against it, and how to exploit similar vulnerabilities.
+        <br></br>
+        <br></br>
+        You will also be able to hide the educational information if you choose, and begin the how-to section.
+        The how-to section will serve as a guide for completing a basic exploit of the vulnerability.
+        There will be a hands on area to work with, as well as a walkthrough provided, so that
+        you are not left without guidance when learning about how to exploit the vulnerability.
+        <br></br>
+        <br></br>
+        Thank you for checking out our project! Happy Hacking!
+        <br></br>
+        <br></br>
+        <br></br>
       </p>
       <h2 className={styles.title}>Vulnerabilities:</h2>
       <ul className={styles.description}>
