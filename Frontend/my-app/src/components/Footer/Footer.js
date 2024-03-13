fa5602cfd7fa3998ec57cc239ede0ae6ff8b66d7
kaneju-osu <81667898+kaneju-osu@users.noreply.github.com>
2024-03-13 13:50:00 -0700

    Add files via upload

--- Frontend/my-app/src/components/Footer/Footer.js
+++ Frontend/my-app/src/components/Footer/Footer.js
@@ -1,14 +1,33 @@
 // Footer.js
-import React from 'react';
-import styles from './Footer.module.css';
+import React from "react";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
+  // Handler function to reset the database.
+  const handleResetDB = async (event) => {
+    event.preventDefault();
+    const url = 'http://localhost:5000/resetDB';
+    try {
+      const response = await fetch(url, {
+        method: "GET", // Now using GET method.
+      });
+      const data = await response.json();
+      return alert(data.message);
+    } catch (error) {
+      console.error("Error resetting database:", error);
+    }
+  };
+
   return (
     <footer className={styles.footer}>
+      {/* Reset DB Button */}
+      <button className={styles.resetButton} onClick={handleResetDB}>
+        Reset Database
+      </button>
       <div className={styles.createdBy}>
-        Created by Devin Black, Lucas Craig Brown, Justice Kane, Stacey Cattel
+        Created by Devin Black, Lucas Craig Brown, Justice Kane, Stacey Cattell
       </div>
-      <div className={styles.date}>Fall 2023</div>
+      <div className={styles.date}>2023-2024</div>
     </footer>
   );
 };
