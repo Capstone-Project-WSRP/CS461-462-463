8a692d7650eba625697f2c138e5dae630d68ce59
kaneju-osu <81667898+kaneju-osu@users.noreply.github.com>
2024-03-13 13:49:33 -0700

    Add files via upload

--- Frontend/my-app/src/components/BrokenAccessControl/BrokenAccessControl.js
+++ Frontend/my-app/src/components/BrokenAccessControl/BrokenAccessControl.js
@@ -1,15 +1,43 @@
-// BrokenAccessControl.js
-import React from 'react';
-import styles from './BrokenAccessControl.module.css';
+import React, { useState } from "react"; // Import useState.
+import Search from "./search";
+import AddUser from "./addUser.jsx";
+import BACdescription from "./BACdescription.jsx";
+import MyComponent from "./walkthrough.jsx";
+import styles from "./BrokenAccessControl.module.css";
+
 const BrokenAccessControl = () => {
-  console.log('BrokenAccessControl component rendered');
+  // State to manage visibility of BACdescription.
+  const [isDescriptionVisible, setDescriptionVisible] = useState(false);
+
+  // Toggle function.
+  const toggleDescription = () => {
+    setDescriptionVisible(!isDescriptionVisible);
+  };
+
+  console.log("BrokenAccessControl component rendered");
   return (
-    <div className={styles.container}> 
+    <div className={styles.container}>
       <h1 className={styles.title}>Broken Access Control</h1>
       <p className={styles.description}>
-        Broken Access Control occurs when a user can perform actions they're not supposed to. 
-        In this simulation....:
+        <Search />
+        <AddUser />
+        <h2>What is Broken Access Control</h2>
+
+        {/* Conditionally rendering "BACdescription" based on "isDescriptionVisible". */}
+        {isDescriptionVisible && <BACdescription />}
+        {/* Button to toggle the description. */}
+        <button onClick={toggleDescription}>
+          {isDescriptionVisible ? "Hide" : "Show"}
+        </button>
+        <br></br>
       </p>
+      <p>
+        <h3>Perform this Attack</h3>
+        <MyComponent />
+      </p>
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
   );
 };
